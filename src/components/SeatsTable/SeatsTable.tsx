20c89499e8a13a9a87ac648dec1fd93c3fa13204
DmitryDemid <dimon.demid15@gmail.com>
2022-11-16 21:20:51 -0500

    Refactoring Config line for customer

--- src/components/SeatsTable/SeatsTable.tsx
+++ src/components/SeatsTable/SeatsTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './SeatsTable.module.css';
 import {ClearSeat} from "../ClearSeat/ClearSeat";
 import {SendEmailToSeatParamsType, SetSeatParamsType, UsersSeatType} from "../../api/api";
@@ -8,12 +8,37 @@ export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAtt
 }
 
 function SeatsTable(props: SeatsTablePropsType) {
-    const {seatsList, onSetSeatEmailClick, onSendEmailToSeat} = props;
+    const {
+        seatsList,
+        onSetSeatEmailClick,
+        onSendEmailToSeat,
+        onConfigureSeatsHandler,
+    } = props;
+
+    const [numberOfSeats, setNumberOfSeats] = useState(5);
+
+    const onNumberOfSeatChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNumberOfSeats(+e.target.value);
+    };
+
+    const onConfigureSeatsClickHandler = () => {
+        onConfigureSeatsHandler(numberOfSeats);
+    };
+
 
     return (
         <div className={s.SeatsTable}>
-            <p>You have <b>{seatsList.length}</b> seats</p>
-            <button>Configure Seats</button>
+            <div className={s.SeatsTable_Header}>
+                <p>You have <b>{seatsList.length}</b> seats</p>
+                <div className={s.SeatsTable_Header_Config}>
+                    <input
+                        type="number"
+                        value={numberOfSeats}
+                        onChange={onNumberOfSeatChangeHandler}
+                    />
+                    <button onClick={onConfigureSeatsClickHandler} className={s.Btn}>Configure Seats</button>
+                </div>
+            </div>
             <table>
                 <tbody>
                 {seatsList.map((seat, i) => {
@@ -50,4 +75,5 @@ type SeatsTablePropsType = {
     seatsList: Array<UsersSeatType | {}>
     onSetSeatEmailClick: (params: SetSeatParamsType) => void
     onSendEmailToSeat: (params: SendEmailToSeatParamsType) => void
+    onConfigureSeatsHandler: (numberOfUsers: number) => void
 }
